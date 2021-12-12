7313c530fd1b2b21234a2ef3606a1c4bca12f935
hamrakulovna <84481766+hamrakulovna@users.noreply.github.com>
2021-12-12 13:50:07 -0600

    Greet by environment variable

--- src/main/java/org/example/controller/HelloController.java
+++ src/main/java/org/example/controller/HelloController.java
@@ -1,12 +1,18 @@
 package org.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
     @GetMapping("/")
-    public String sayHello() {
+    public String sayHello(Model model) {
+        String name = System.getenv("Visitor_Name");
+        if(name ==null) {
+            name = "World";
+        }
+        model.addAttribute("name", name);
         return "index";
     }
 }
